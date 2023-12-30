ef7fc16c39953c086594a86ebfd9f02a3950310a
akifakif32 <mhmtkfdrn@gmail.com>
2023-12-30 13:04:36 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,1 @@
 git clone https://github.com/akifakif32/twrp-device_transsion_mt6789-common.git -b fox_12.1 device/transsion/mt6789-common
-git clone https://android.googlesource.com/platform/external/gflags/ -b android-12.1.0_r4 external/gflags
